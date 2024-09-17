a9e2c379983ff8369b89f687b4a0bcd7a1a477e8
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-09-17 10:57:03 +0300

    add simple express app with some middlewars

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+    next(err);
+});
+
+module.exports = app;
